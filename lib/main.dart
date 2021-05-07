3c68a4de1b2365b4d571a3781897287e016f516e
Junior Medehou <junior.medehou00@hotmail.com>
2021-05-07 10:49:06 +0100

    [UPDATE] on title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Epitech Chatbot',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
